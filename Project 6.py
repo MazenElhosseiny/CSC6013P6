7b42d14ba386c609ccd2e9ff8910f3a44dba1e8c
MazenElhosseiny <mhos2014@gmail.com>
2025-08-13 20:48:02 -0700

    Add files via upload

---
+++ Project 6.py
@@ -0,0 +1,21 @@
+def findKthSmallest(nums, k):
+    k = k-1
+
+    def quickSelect(l, r):
+        pivot, p = nums[r], l
+        for i in range(l,r):
+            if nums[i] <= pivot:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
+        nums[p], nums[r] = nums[r], nums[p]
+
+        if p > k:
+            return quickSelect(1, p-1)
+        elif p < k:
+            return quickSelect(p+1, r)
+        else:
+            return nums[p]
+
+    return quickSelect(0, len(nums) - 1)
+
+#def main():
