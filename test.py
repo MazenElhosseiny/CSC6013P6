9d07c6f085837b46fcfcf5433953ebc4572db11c
MazenElhosseiny <mhos2014@gmail.com>
2025-08-14 09:51:42 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+import random
+
+def findKthSmallest(nums, k):
+    k = k-1
+
+    def quickSelect(l, r):
+        pivot, p = nums[r], l
+        for i in range(l,r):
+            if nums[i] <= pivot:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
+        nums[p], nums[r] = nums[r], nums[p]
+
+        if p > k:
+            return quickSelect(1, p-1)
+        elif p < k:
+            return quickSelect(p+1, r)
+        else:
+            return nums[p]
+
+    return quickSelect(0, len(nums) - 1)
+
+def main():
+    randomList = []
+    for i in range(1000):
+        randomList.append(random.randint(0,1000))
+
+    while True:
+        choice = input("Enter an integer value for the kth Smallest element to find in an unsorted array: ")
+        if not choice:
+            print("No input received.")
+        else:
+            try:
+                choice_int = int(choice)
+                break
+            except ValueError:
+                print("Please enter an integer value.")
+    randomList.sort()
+    print(randomList)
+    print(f"The {choice}th smallest element in the array is:", findKthSmallest(randomList, choice_int))
+
+main()
